1b0f528ed90ea6de9d4eabb1838adb392dff27eb
hamzamain <mohammadmainuddin211@gmail.com>
2022-11-15 11:26:10 +0600

    new

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("data.json");
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selected = news.filter((ns) => ns.category_id == id);
+  res.send(selected);
 });
 
 app.get("/news-categories", (req, res) => {
